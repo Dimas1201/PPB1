dc9ea20c8c3f24e0537671493b9cc3d4afdee77d
Dimas1201 <dzaki120104@gmail.com>
2025-03-25 07:57:08 +0700

    Initial commit

---
+++ lib/widgets/gif_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GifWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('lib/assets/tired.gif');// Replace with actual path
+  }
+}
